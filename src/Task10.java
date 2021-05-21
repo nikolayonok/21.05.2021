59a4724bd1669696ec05dcd87ade8f6f9b2de291
nikolayonok <nikolayonokk@yandex.ru>
2021-05-21 21:19:29 +0300

    task10

--- src/Task10.java
+++ src/Task10.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Task10 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите сторону квадрата ");
+        int a = sc.nextInt();
+        for (int i = 0; i <= a; i++) {
+            for (int j = 0; j <= a; j++) {
+                if (i == 0 || i == a || j == 0 || j == a) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println(' ');
+        }
     }
 }
+
