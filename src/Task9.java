1a2c32e9d1d9cd332f8b686871bd310e4a9de371
nikolayonok <nikolayonokk@yandex.ru>
2021-05-21 21:01:44 +0300

    task9

--- src/Task9.java
+++ src/Task9.java
@@ -5,6 +5,11 @@ public class Task9 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите сторону квадрата ");
         int a = sc.nextInt();
-
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println(' ');
+        }
     }
 }
